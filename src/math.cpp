82a4b00e820801aaf3717fdcce3639f6887693ff
Ekler25 <134164161+ekler25@users.noreply.github.com>
2026-03-29 00:08:58 +0100

    more methods n' stuff

--- src/math.cpp
+++ src/math.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <math.hpp>
 
+int Math::Interval::Size() {
+    return this->Max - this->Min;
+}
+
 bool Math::Interval::Contains(double Num) {
     return 
         (this->ClosedLeft ? 
@@ -9,4 +13,25 @@ bool Math::Interval::Contains(double Num) {
         (this->ClosedRight ? 
             (this->Max > Num) : 
             (this->Max >= Num));
+}
+
+bool Math::Interval::Contains(Interval& Intv) {
+    return 
+        (Intv.ClosedLeft ? 
+            (Intv.Min >= this->Min) : 
+            (Intv.Min > this->Min)) && 
+        (Intv.ClosedRight ? 
+            (Intv.Max <= this->Max) : 
+            (Intv.Max < this->Max));
+}
+
+void Math::Interval::Print(bool Color) {
+    if (Color)
+        std::cout << "\x1b[0m(\x1b[96m" <<
+            this->Min << "\x1b[0m, \x1b[96m" <<
+            this->Max << "\x1b[0m)\n";
+    else
+        std::cout << "(" <<
+            this->Min << ", " <<
+            this->Max << ")\n";
 }
