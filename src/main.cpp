c547efe7eeecfd3d00c29f5593a904d2f0bb984e
Ekler25 <134164161+ekler25@users.noreply.github.com>
2026-03-29 00:08:39 +0100

    demo

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,12 @@
 #include <math.hpp>
 
 Math::Interval A = Math::Interval(0, 10, false, false);
+Math::Interval B = Math::Interval(3, 13, false, false);
+Math::Interval C = Math::Interval(-Math::INF, Math::INF, false, false);
 
 int main() {
-    std::cout << A.Contains(-5) << "\n";
-    std::cout << A.Contains(0) << "\n";
-    std::cout << A.Contains(5) << "\n";
-    std::cout << A.Contains(10) << "\n";
-    std::cout << A.Contains(20) << "\n";
+    std::cout << A.Contains(B) << "\n";
+    A.Print();
+    B.Print();
+    C.Print();
 }
