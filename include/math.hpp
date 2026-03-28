b4db3eb83708689a8a50b4837a29628ca3d53613
Ekler25 <134164161+ekler25@users.noreply.github.com>
2026-03-29 00:09:50 +0100

    gotta write your code twice

--- include/math.hpp
+++ include/math.hpp
@@ -1,14 +1,32 @@
 #pragma once
 
+#include <limits>
+
 namespace Math {
+    static constexpr double PI   = 3.141592653589793;
+    static constexpr double TAU  = PI * 2;
+    static constexpr double SQR2 = 1.772453850905516;
+    static constexpr double E    = 2.718281828459045;
+    static constexpr double INF = std::numeric_limits<double>::infinity();
+
     struct Interval {
         double Min;
         double Max;
-        bool ClosedLeft = false;
-        bool ClosedRight = false;
+        bool   ClosedLeft;
+        bool   ClosedRight;
+        bool   Whole;
 
-        Interval(double Mn, double Mx, bool ClL = false, bool ClR = false) 
-            : Min(Mn), Max(Mx), ClosedLeft(ClL), ClosedRight(ClR) {};
+        Interval(double Mn, double Mx, bool ClL = false, bool ClR = false, bool Wh = false) 
+            : Min(Mn), Max(Mx), ClosedLeft(ClL), ClosedRight(ClR), Whole(Wh) {};
+
+        int  Size();
         bool Contains(double Num);
+        bool Contains(Interval& Intv);
+        void Print(bool Color = true);
     };
+
+    const Interval realInterval    = Interval(-INF, INF);
+    const Interval integerInterval = Interval(-INF, INF, false, false, false);
+    const Interval wholeInterval   = Interval(0, INF, false, false, true);
+    const Interval naturalInterval = Interval(1, INF, false, false, true);
 }
