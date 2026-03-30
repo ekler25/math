c1ba005f6feace2465bad6690d360a8de7a2eb00
Ekler25 <134164161+ekler25@users.noreply.github.com>
2026-03-30 21:15:20 +0200

    an example of collections

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <math.hpp>
 
-Math::Interval A = Math::Interval(0, 10, false, false);
-Math::Interval B = Math::Interval(3, 13, false, false);
-Math::Interval C = Math::Interval(-Math::INF, Math::INF, false, false);
+Math::Collection A;
+Math::Collection B;
+Math::Collection C;
 
 int main() {
-    std::cout << A.Contains(B) << "\n";
+    A.Set({0, 10, 55, 30, 1});
+    B.Set({10, 55, 1});
+    C.Set({5, 7});
     A.Print();
-    B.Print();
-    C.Print();
+    std::cout << A.Contains(B) << "\n";
+    std::cout << A.Contains(C) << "\n";
+    std::cout << A.Size();
 }
