5fa54a2eebbc6f46e0520c7e1db29a4e38b4b654
Ekler25 <134164161+ekler25@users.noreply.github.com>
2026-04-13 21:20:51 +0200

    exp()

--- src/math/functions.cpp
+++ src/math/functions.cpp
@@ -3,3 +3,17 @@
 double Math::abs(double Num) {
     return Num < 0 ? -Num : Num;
 }
+
+double Math::exp(double Num) {
+    double Term = 1.0;
+    double Sum = 1.0;
+
+    for (int i = 1; i < 100; i++) {
+        Term *= Num / i;
+        Sum += Term;
+
+        if (Term < 1e-15 && Term > -1e-15) break;
+    }
+
+    return Sum;
+}
